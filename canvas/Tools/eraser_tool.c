ef684ca0de4d931ff2a40f6e7caab10b11b86025
Yash <8f34yashjadhav@gmail.com>
2025-10-24 18:45:43 +0530

    temp commit

--- canvas/Tools/eraser_tool.c
+++ canvas/Tools/eraser_tool.c
@@ -1,32 +1,33 @@
 #include "eraser_tool.h"
 #include "SDL3/SDL_log.h"
 #include "SDL3/SDL_render.h"
+#include "pen_tool.h"
 #include <SDL3/SDL.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-int eraser_size = 5;
 
 void point_eraser(Pixel **pixel_storage_ptr, float mouse_x, float mouse_y,
                   int rect_no) {
 
+  float eraser_size = 5;
   // getting double pointer as later realloc has to be used for moving erased
   // memory to stack
   Pixel *pixel_storage = *pixel_storage_ptr;
-
+  int erased = 0;
   // general equation of circle to know if point is inside or outside
   for (int i = 0; i < rect_no; i++) {
     Pixel *curr_point = &pixel_storage[i];
     int x = curr_point->pixel.x;
     int y = curr_point->pixel.y;
-    float S =
-        x * x + y * y - (2 * mouse_x * curr_point->pixel.x) -
-        (2 * mouse_y * curr_point->pixel.y) +
-        (mouse_x * mouse_x + mouse_y * mouse_y - eraser_size * eraser_size);
-    if (S <= 0) {
-      SDL_Log("Point inside or on");
+    float dx = curr_point->pixel.x - mouse_x;
+    float dy = curr_point->pixel.y - mouse_y;
+    if (dx * dx + dy * dy <= eraser_size * eraser_size) {
       curr_point->visible = false;
+      erased++;
     }
   }
+  /*   if (erased > 0)
+      SDL_Log("Erased %d pixels at (%.2f, %.2f)", erased, mouse_x, mouse_y); */
 }
